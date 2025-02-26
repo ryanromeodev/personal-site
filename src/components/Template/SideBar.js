276a76ee6d5354a3b548e92470795dba0e66d2e4
Ryan Romeo <ryanromeo10@gmail.com>
2025-02-27 00:34:22 +0100

    clean up

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -44,7 +44,7 @@ const SideBar = () => (
     <section id="footer">
       <ContactIcons />
       <p className="copyright">
-        &copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.
+      &copy; <Link to="/"> Ryan Romeo.</Link>
       </p>
     </section>
   </section>
