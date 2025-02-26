80035af8022ef9466be83466fbc0ff59fbc83fb7
Ryan Romeo <ryanromeo10@gmail.com>
2025-02-26 18:16:18 +0100

    updated spaces

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,12 +1,6 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
-
-//import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
-//import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-//import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-//import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
-
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
