3d5e862c0e55ad1b067715b284eea537d394012b
Ryan Romeo <ryanromeo10@gmail.com>
2025-02-27 01:31:05 +0100

    typo

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -6,19 +6,19 @@ const degrees = [
     year: 2023,
     fos: '**Specialization**: Intelligent Sensors and Instrumentation',
     highlights: [
-      'Modelling and documentation of 3 - phase measurement'
+      'Modelling and documentation of 3 - phase measurement '
     + 'system using LabVIEW and Arduino Mega 2560.',
-      'Verification of the output Characteristics of Transistor'
+      'Verification of the output Characteristics of Transistor '
     + '(BC547B) using LabVIEW',
-      'Projects were done and documented to find the concentration of sugar'
-    + 'in a sample sugar solution and used python libraries like numpy, matploitlib,'
+      'Projects were done and documented to find the concentration of sugar '
+    + 'in a sample sugar solution and used python libraries like numpy, matploitlib, '
     + 'scipy_curvefit to validate the results.',
-      'Certified with a Udemy course on Microcontroller'
-    + 'Embedded C Programming and documented a seminar'
-    + 'done on the topic of the use case of python in'
+      'Certified with a Udemy course on Microcontroller '
+    + 'Embedded C Programming and documented a seminar '
+    + 'done on the topic of the use case of python in '
     + 'Instrumentation and measurement systems.',
-      'Currently working on Flutter application of a sudoku'
-    + 'solver from scratch',
+      'Currently working on Flutter application of a sudoku '
+    + 'solver from scratch. ',
     ],
   },
   {
@@ -28,12 +28,12 @@ const degrees = [
     year: 2021,
     fos: '**Specialization**: Electrical and Electronics Engineering',
     highlights: [
-      'Created a plant maintenance application using Java and'
+      'Created a plant maintenance application using Java and '
       + 'later migrated the application to Flutter.',
-      'Attended certification courses on programming in java, sensors and'
-      + 'actuators from Indian Institute of Technology',
-      'Designed a prototype to control the on-load tap changing for HVDC'
-      + ' converter transformer.',
+      'Attended certification courses on programming in java, sensors and '
+      + 'actuators from Indian Institute of Technology ',
+      'Designed a prototype to control the on-load tap changing for HVDC '
+      + 'converter transformer.',
     ],
   },
 ];
