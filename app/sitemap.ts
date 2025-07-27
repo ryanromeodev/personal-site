27d17d1da2af88c38e0207d69132b75087398ded
Ryan Romeo <ryanromeo10@gmail.com>
2025-07-27 20:15:24 +0200

    refactor

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export const dynamic = 'force-static';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ryan-romeo.github.io';
+  const baseUrl = 'https://ryan-romeo.github.io/personal-site';
   const currentDate = new Date();
 
   return [
