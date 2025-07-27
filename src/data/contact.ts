07b39cd3c52a3b848e65277b8a7d8d51a26f2449
Ryan Romeo <ryanromeo10@gmail.com>
2025-07-27 21:39:01 +0200

    cleaning errors

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,10 +1,6 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 export interface ContactItem {
@@ -15,20 +11,20 @@ export interface ContactItem {
 
 const data: ContactItem[] = [
   {
-      link: 'https://github.com/oreymaonr',
-      label: 'Github',
-      icon: faGithub,
-    },
-    {
-      link: 'https://www.linkedin.com/in/ryan-romeo-aa880821a/',
-      label: 'LinkedIn',
-      icon: faLinkedinIn,
-    },
-    {
-      link: 'mailto:ryanromeo10@gmail.com',
-      label: 'Email',
-      icon: faEnvelope,
-    },
+    link: 'https://github.com/oreymaonr',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.linkedin.com/in/ryan-romeo-aa880821a/',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'mailto:ryanromeo10@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
 ];
 
 export default data;
--- src/data/routes.ts
+++ src/data/routes.ts
@@ -7,7 +7,7 @@ export interface Route {
 const routes: Route[] = [
   {
     index: true,
-    label: "Ryan Romeo",
+    label: 'Ryan Romeo',
     path: '/',
   },
   {
