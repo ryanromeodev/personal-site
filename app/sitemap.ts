e982b4f5fbc568a6de6eb15e0424cb648c214148
Ryan Romeo <ryanromeo10@gmail.com>
2025-07-27 19:53:28 +0200

    renam and package update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export const dynamic = 'force-static';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://mldangelo.com';
+  const baseUrl = 'https://oreymaonr.github.io';
   const currentDate = new Date();
 
   return [
