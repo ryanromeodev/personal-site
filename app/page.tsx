938e5cda885b302f03446f727e3e65815f3557a3
Ryan Romeo <ryanromeo10@gmail.com>
2025-07-30 14:06:28 +0200

    text edit on sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,10 @@ export default function HomePage() {
           </div>
         </header>
         <p>
-          {' '}
           Welcome to my website. Please feel free to read more <Link href="/about">about me</Link>,
-          or you can check out my <Link href="/resume">resume</Link>,{' '}
+          or you can check out my <Link href="/resume">resume</Link>,
           <Link href="/projects">projects</Link>, view my <Link href="/stats">statistics</Link>,
-          or <Link href="/contact">contact</Link> me.
+          or/and <Link href="/contact">contact</Link> me.
         </p>
         <p>
           {' '}
