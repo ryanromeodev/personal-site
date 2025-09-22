70d4fd4b150d1b20a9c03563f1018652fa6d8946
Ryan Romeo <ryanromeo10@gmail.com>
2025-09-23 00:09:37 +0200

    eslint linting

--- app/page.tsx
+++ app/page.tsx
@@ -20,21 +20,17 @@ export default function HomePage() {
               <Link href="/about">About this site</Link>
             </h2>
             <p>
-              This site is a snapshot of who I am: my projects, technical skills, professional
-              experience, and hobbies
+              Welcome to my website. Please feel free to read more{' '}
+              <Link href="/about">about me</Link>, or you can check out my{' '}
+              <Link href="/resume">resume</Link>,<Link href="/projects">projects</Link>, view my{' '}
+              <Link href="/stats">statistics</Link>, and/or <Link href="/contact">contact</Link> me.
+            </p>
+            <p>
+              {' '}
+              Source available <a href="https://github.com/ryanromeodev/personal-site">here</a>.
             </p>
           </div>
         </header>
-        <p>
-          Welcome to my website. Please feel free to read more <Link href="/about">about me</Link>,
-          or you can check out my <Link href="/resume">resume</Link>,
-          <Link href="/projects">projects</Link>, view my <Link href="/stats">statistics</Link>,
-          and/or <Link href="/contact">contact</Link> me.
-        </p>
-        <p>
-          {' '}
-          Source available <a href="https://github.com/ryanromeodev/personal-site">here</a>.
-        </p>
       </article>
     </PageWrapper>
   );
